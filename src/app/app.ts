79a5cb054bccf02b4cc371d5d2ea535bbb3fa068
Yokesh KR <yokesh.kr@stellaripl.com>
2025-12-16 18:38:23 +0530

    chore: router task initial check

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
