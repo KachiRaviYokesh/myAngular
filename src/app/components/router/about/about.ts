910b9a1ea6915eaead3b228c105305c0d29b0ac4
Yokesh KR <yokesh.kr@stellaripl.com>
2025-12-17 21:56:23 +0530

    fix: activate is not changed once refresh the page

--- src/app/components/router/about/about.ts
+++ src/app/components/router/about/about.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -10,21 +9,11 @@ import { filter } from 'rxjs';
 export class About {
   private router:Router = inject(Router);
   private currentUrl:ActivatedRoute = inject(ActivatedRoute);
-  activeRoute!:string;
-  
-  constructor() {
-    this.router.events
-      .pipe(filter(eve => eve instanceof NavigationEnd))
-      .subscribe(() => {
-        this.activeRoute =
-          this.currentUrl.firstChild?.snapshot.url[0]?.path || 'company';
-      });
-  }
 
-  // If we are using this method then we are facing change.detection(Multiple time calling)
-  // get activeRoute(): string {
-  //   return this.currentUrl.firstChild?.snapshot.url[0]?.path || 'company';
-  // }
+  // If we are using this method then we are facing change.detection (Multiple time calling)
+  get activeRoute(): string {
+    return this.currentUrl.firstChild?.snapshot.url[0]?.path || 'company';
+  }
 
   secondLevelBtn(path: string) {
     this.router.navigate([path], {
--- src/app/services/common-service.ts
+++ src/app/services/common-service.ts
@@ -17,12 +17,31 @@ export class CommonService {
       team: 'Frontend'
     },
     {
-      id: 1,
+      id: 3,
       team: 'Backend'
     },
     {
-      id: 1,
+      id: 4,
       team: 'Testing'
     },
-  ]  
+  ];
+
+  teamMembers = [
+    {
+      id: 1,
+      members: ['Kapil Dev', 'Gavaskar', 'Krishnamachari Shreekanth']
+    },
+    {
+      id: 2,
+      members: ['Sourav Ganguly', 'Sachin Tendulkar', 'Yuvraj Singh']
+    },
+    {
+      id: 3,
+      members: ['MS Dhoni', 'Suresh Raina', 'Ravindra Jadeja']
+    },
+    {
+      id: 4,
+      members: ['Rohit Sharma', 'Virat Kholi', 'Jasprit Bumrah']
+    },
+  ]
 }
