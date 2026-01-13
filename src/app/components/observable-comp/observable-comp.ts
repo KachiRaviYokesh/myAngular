e1b8e94879b8ad43e878221039901fb667d3bf40
Yokesh KR <yokesh.kr@stellaripl.com>
2026-01-13 16:12:01 +0530

    refactor: $ symbol added in observable variable

--- src/app/components/observable-comp/observable-comp.ts
+++ src/app/components/observable-comp/observable-comp.ts
@@ -9,27 +9,28 @@ import { from, map, Observable, of } from 'rxjs';
 })
 export class ObservableComp implements OnInit {
   firstReceivedData!:number[];
-  secondReceivedData!:number[];
-  thirdReceivedData!:string[];
-
-  firstObsVariable = new Observable<number[]>((firstData)=>{
+  firstObsVariable$ = new Observable<number[]>((firstData)=>{
     firstData.next([1,3,5,7,9]);
     // data.error('Error1');
     firstData.complete();
   });
-  secondObsVariable = new Observable<number[]>((secondData)=>{
+
+  secondReceivedData!:number[];
+  secondObsVariable$ = new Observable<number[]>((secondData)=>{
     secondData.next([2,4,6,8,10]);
     // secondData.error('Error2');
     secondData.complete();
   });
-  thirdObsVariable = new Observable<string[]>((thirdData)=> {
+
+  thirdReceivedData!:string[];
+  thirdObsVariable$ = new Observable<string[]>((thirdData)=> {
     thirdData.next(['A','B','C','D','E']);
     // thirdData.error('Error3');
     thirdData.complete();
   });
 
   ngOnInit(): void {
-    this.firstObsVariable.subscribe({
+    this.firstObsVariable$.subscribe({
       next: (firstRes) => {
         this.firstReceivedData = firstRes;
         console.log(firstRes);        
@@ -39,7 +40,7 @@ export class ObservableComp implements OnInit {
       },
       complete: () => {
         console.log('First Completed');
-        this.secondObsVariable.subscribe({
+        this.secondObsVariable$.subscribe({
           next: (secondRes) => {
             this.secondReceivedData = secondRes;
             console.log(secondRes);
@@ -53,7 +54,7 @@ export class ObservableComp implements OnInit {
   }
 
   protected thirdRespondData() {
-    this.thirdObsVariable.subscribe({
+    this.thirdObsVariable$.subscribe({
       next:(thirdRes)=>{
         this.thirdReceivedData = thirdRes;
         console.log(thirdRes);        
@@ -65,9 +66,9 @@ export class ObservableComp implements OnInit {
   }
 
   firstOfObsData:number[] = [];
-  firstOfObs = of([1,2,3,4],[5,6,7,8]);
+  firstOfObs$ = of([1,2,3,4],[5,6,7,8]);
   protected firstOfRespondData() {
-    this.firstOfObs.subscribe({
+    this.firstOfObs$.subscribe({
       next:(firstOfRes) => {
         firstOfRes.forEach((d)=>{
           this.firstOfObsData.push(d);
@@ -81,9 +82,9 @@ export class ObservableComp implements OnInit {
   }
 
   firstFromObsData:string[] = [];
-  firstFromObs = from('YOKESH');
+  firstFromObs$ = from('YOKESH');
   protected firstFromRespondData() {
-    this.firstFromObs.subscribe({
+    this.firstFromObs$.subscribe({
       next:(firstFromRes) => {
         this.firstFromObsData.push(firstFromRes);
         console.log(firstFromRes);        
